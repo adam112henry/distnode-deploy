1d196b2f018d0129154847b4bbfa2f11e445736e
Adam Henry <adam@aplossoftware.com>
2025-05-16 08:35:37 -0500

    Application files

--- server.js
+++ server.js
@@ -3,14 +3,15 @@
 // npm install fastify@3.2
 const server = require('fastify')();
 const HOST = process.env.HOST || '127.0.0.1';
+/* istanbul ignore next */
 const PORT = process.env.PORT || 8000;
 const Recipe = require('./recipe.js');
 
 server.get('/', async (req, reply) => {
     return "Hello from Distributed Node.js!";
 });
-server.get('/recipe/:id', async (req, reply) => {
-    const recipe = new Recipe(reg.params.id);
+server.get('/recipes/:id', async (req, reply) => {
+    const recipe = new Recipe(req.params.id);
     await recipe.hydrate();
     return recipe;
 });
